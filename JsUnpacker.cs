8525d97bfbacd8d003f5e2b0dc2c13ede6636da1
Thismaker <smithkare47@gmail.com>
2024-12-13 13:01:30 +0300

    Add project files.

---
+++ JsUnpacker.cs
@@ -0,0 +1,115 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Thismaker.JsUnpacker
+{
+    /// <summary>
+    /// Unpacks packed JavaScript code
+    /// </summary>
+    public partial class JsUnpacker
+    {
+        /// <summary>
+        /// Detects if the source contains packed JavaScript code
+        /// </summary>
+        public static bool Detect(string source)
+        {
+            return PackedRegex().IsMatch(source);
+        }
+
+        /// <summary>
+        /// Detects if the supplied sources contain packed JavaScript code
+        /// </summary>
+        public static List<string> Detect(params string[] scriptBlocks)
+        {
+            return scriptBlocks.Where(Detect).ToList();
+        }
+
+        /// <summary>
+        /// Detects if the supplied sources contain packed JavaScript code
+        /// </summary>
+        public static List<string> Detect(IEnumerable<string> scriptBlocks)
+        {
+            return scriptBlocks.Where(Detect).ToList();
+        }
+
+        /// <summary>
+        /// Attempts to unpack the supplied packed JavaScript code
+        /// </summary>
+        public static IEnumerable<string> Unpack(string scriptBlock)
+        {
+            if (!Detect(scriptBlock))
+            {
+                return [];
+            }
+            else
+            {
+                return PerformUnpack(scriptBlock);
+            }
+        }
+
+        /// <summary>
+        /// Unpacks available packed JavaScript code in the supplied <paramref name="scriptBlocks"/>
+        /// </summary>
+        public static List<string> Unpack(IEnumerable<string> scriptBlocks)
+        {
+            return scriptBlocks.SelectMany(Unpack).ToList();
+        }
+
+        /// <summary>
+        /// Attempts to unpack the supplied JavaScript code and returns a single string or null if no packed code was detected.
+        /// </summary>
+        public static string? UnpackAndCombine(string scriptBlock)
+        {
+            var unpacked = Unpack(scriptBlock).ToList();
+
+            if (unpacked.Count == 0)
+            {
+                return null;
+            }
+
+            return string.Join("\n", Unpack(scriptBlock));
+        }
+
+        private static IEnumerable<string> PerformUnpack(string scriptBlock)
+        {
+            var matches = PackedExtractRegex().Matches(scriptBlock);
+
+            foreach (Match match in matches)
+            {
+                var payload = match.Groups[1].Value;
+                var symTab = match.Groups[4].Value.Split('|');
+                var radix = int.Parse(match.Groups[2].Value);
+                var count = int.Parse(match.Groups[3].Value);
+
+                var unbaser = new Unbaser(radix);
+
+                var unpacked = UnpackReplaceRegex().Replace(payload, m =>
+                {
+                    var word = m.Value;
+                    var unbase = unbaser.Unbase(word);
+
+                    if (unbase < count)
+                    {
+                        return symTab[unbase];
+                    }
+                    else
+                    {
+                        return word;
+                    }
+                });
+
+                yield return unpacked;
+
+            }
+        }
+
+        [GeneratedRegex("eval[(]function[(]p,a,c,k,e,[r|d]?", RegexOptions.IgnoreCase | RegexOptions.Multiline, "en-GB")]
+        
+        private static partial Regex PackedRegex();
+        
+        [GeneratedRegex("[}][(]'(.*)', *(\\d+), *(\\d+), *'(.*?)'[.]split[(]'[|]'[)]", RegexOptions.IgnoreCase | RegexOptions.Multiline, "en-GB")]
+        private static partial Regex PackedExtractRegex();
+        
+        [GeneratedRegex("\\b\\w+\\b", RegexOptions.IgnoreCase | RegexOptions.Multiline, "en-GB")]
+        private static partial Regex UnpackReplaceRegex();
+    }
+}
